70cf480f985b717614696d4a8a0349acf67e88d8
Lucky Denis N. Cayanan <lucky_cayanan@cathaypacific.com>
2022-11-01 21:30:50 +0800

    Implement sftp

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,18 +5,46 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+
+import net.schmizz.sshj.SSHClient;
+import net.schmizz.sshj.sftp.SFTPClient;
+import net.schmizz.sshj.transport.verification.PromiscuousVerifier;
 
 @RestController
 @SpringBootApplication
 public class DemoApplication {
 
+    private String remoteHost = "mft-int-pat.ete.cathaypacific.com";
+    private String username = "etechlog_azure_ete";
+    private String password = "etechlog_azure_pwd";
+    private String localFile = "src/main/resources/input.txt";
+    private String remoteDir = "remote_sftp_test/";
+
     @RequestMapping("/")
-    String home() {
-        return "Hello World!";
+    String home() throws IOException {
+        this.whenUploadFileUsingSshj_thenSuccess();
+        return "Hello World Lucky!!!";
     }
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
+    }
+
+    private SSHClient setupSshj() throws IOException {
+        SSHClient client = new SSHClient();
+        client.addHostKeyVerifier(new PromiscuousVerifier());
+        client.connect(remoteHost);
+        client.authPassword(username, password);
+        return client;
+    }
+
+    public void whenUploadFileUsingSshj_thenSuccess() throws IOException {
+        SSHClient sshClient = setupSshj();
+        SFTPClient sftpClient = sshClient.newSFTPClient();
+        sftpClient.put(localFile, remoteDir + "sshjFile.txt");
+        sftpClient.close();
+        sshClient.disconnect();
     }
 
 }
