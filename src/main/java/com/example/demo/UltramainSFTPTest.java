d2e81e9d16b314b7f4501e575779c55e42cf7bbb
Lucky Denis N. Cayanan <lucky_cayanan@cathaypacific.com>
2022-11-11 00:34:58 +0800

    new code from Shweta

--- src/main/java/com/example/demo/UltramainSFTPTest.java
+++ src/main/java/com/example/demo/UltramainSFTPTest.java
@@ -8,26 +8,29 @@ import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 import java.io.*;
 import java.util.EnumSet;
 
 @Component
+@Order(1)
 public class UltramainSFTPTest implements CommandLineRunner {
     private static final String SFTP_USERNAME = "etechlog_azure_ete";
     private static final String SFTP_HOST = "mft-int-pat.ete.cathaypacific.com";
+
     private static final String SFTP_PASSWORD = "etechlog_azure_pwd";
     private static final short SFTP_PORT = 22;
 
     public static final Logger LOGGER = LoggerFactory.getLogger(UltramainSFTPTest.class);
 
-    private OutputStream getOutputStream(SFTPClient client, String fileName) throws IOException {
-        String remoteFilePath = "etlg001/in/" + fileName;
-        LOGGER.info("opening remote file {}", remoteFilePath);
-        RemoteFile remoteFile = client.open(remoteFilePath, EnumSet.of(OpenMode.WRITE, OpenMode.CREAT, OpenMode.TRUNC));
-        return remoteFile.new RemoteFileOutputStream();
-    }
+    // private OutputStream getOutputStream(SFTPClient client, String fileName) throws IOException {
+    //     String remoteFilePath = "etlg001/in/" + fileName;
+    //     LOGGER.info("opening remote file {}", remoteFilePath);
+    //     RemoteFile remoteFile = client.open(remoteFilePath, EnumSet.of(OpenMode.WRITE, OpenMode.CREAT, OpenMode.TRUNC));
+    //     return remoteFile.new RemoteFileOutputStream();
+    // }
 
     public void transfer() {
         try (final SSHClient ssh = new SSHClient()) {
@@ -47,14 +50,17 @@ public class UltramainSFTPTest implements CommandLineRunner {
                 LOGGER.info("successfully authenticated with password");
             }
 
-            String fileName = "B-USI-202210280305-CX777.xml";
+            String fileName = "/app/target/demo-0.0.1-SNAPSHOT.jar";
 
             try (SFTPClient sftp = ssh.newSFTPClient()) {
-                try (InputStream is = UltramainSFTPTest.class.getClassLoader().getResourceAsStream(fileName); OutputStream os = getOutputStream(sftp, fileName)) {
-                    LOGGER.info("Copying file {} to sftp", fileName);
-                    IOUtils.copy(is, os);
-                    LOGGER.info("Successfully copied file {} to sftp", fileName);
-                }
+                LOGGER.info("Copying file {} to sftp using upload function", fileName);
+                sftp.getFileTransfer().upload(fileName, "etlg001/in/demo.jar");
+                LOGGER.info("Successfully copied file {} to sftp using upload function", fileName);
+                // try (InputStream is = UltramainSFTPTest.class.getClassLoader().getResourceAsStream(fileName); OutputStream os = getOutputStream(sftp, fileName)) {
+                //     LOGGER.info("Copying file {} to sftp", fileName);
+                //     IOUtils.copy(is, os);
+                //     LOGGER.info("Successfully copied file {} to sftp", fileName);
+                // }
             }
         } catch (UserAuthException uae) {
             LOGGER.error("sftp://{}@{}:{} authentication failed", SFTP_USERNAME, SFTP_HOST, SFTP_PORT, uae);
@@ -66,5 +72,10 @@ public class UltramainSFTPTest implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         this.transfer();
+    }
+
+    public static void main(String args[]) throws Exception {
+        UltramainSFTPTest test = new UltramainSFTPTest();
+        test.run(args);
     }
 }
